4cc5fd339e6e94f5e9f20bb2e3e3727b0629f12f
Ismailov Timur <i.timka92@gmail.com>
2022-05-22 12:30:22 +0200

    fix server.js for test workers

--- server.js
+++ server.js
@@ -2,13 +2,34 @@
 const express = require("express");
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const path = require("path");
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const fs = require("fs");
 
 const app = express();
 
 app.use(express.static(path.join(__dirname, "/dist")));
 
 app.get("/*", (req, res) => {
-    res.sendFile(path.join(__dirname, "/dist/index.html"));
+    const indexHTML = path.join(__dirname, "/dist/index.html");
+
+    fs.open(indexHTML, "r", (err, fd) => {
+        if (err) {
+            if (err.code === "ENOENT") {
+                res.send("Hello");
+                return;
+            }
+
+            throw err;
+        }
+
+        try {
+            res.sendFile(indexHTML);
+        } finally {
+            fs.close(fd, (err) => {
+                if (err) throw err;
+            });
+        }
+    });
 });
 
 app.listen("3000");
